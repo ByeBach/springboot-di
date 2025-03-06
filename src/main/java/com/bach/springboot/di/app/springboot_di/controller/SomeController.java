5a7a6beb091680564ff5a002b388e4e53af7f40f
Bach <byebachgm@gmail.com>
2025-03-06 17:00:01 -0300

    Agregar controlador y servicio para gestionar productos

---
+++ src/main/java/com/bach/springboot/di/app/springboot_di/controller/SomeController.java
@@ -0,0 +1,28 @@
+package com.bach.springboot.di.app.springboot_di.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bach.springboot.di.app.springboot_di.models.Product;
+import com.bach.springboot.di.app.springboot_di.services.ProductService;
+
+@RestController
+@RequestMapping("/api")
+public class SomeController {
+    
+    private ProductService service = new ProductService();
+    
+    @GetMapping
+    public List<Product> list(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id){
+        return service.findById(id);
+    }
+}
